5e2ef0cd9f6b8ef37b0da33bd96761286f54387d
Ayush5317C <ayushacharya.htd057@gmail.com>
2023-01-23 19:08:59 +0545

    Added searching functionality of users

--- public/scripts/admin.js
+++ public/scripts/admin.js
@@ -1,15 +1,35 @@
+//Variables
 let addUserModal = document.querySelector(".addUserModal");
+let searchInput = document.querySelector(".searchInput");
 let canceladdUserModalBtn = document.querySelector(".addUserModal .cancelBtn");
 let addBtn = document.querySelector(".addBtn");
-addBtn.addEventListener("click", ()=>{
-    if(addBtn.classList.contains("addStudent")){
-        document.querySelector("#studentRadio").checked = true;
+let tableRow = document.querySelectorAll(".userListTable tbody tr");
+
+//Functions
+function filter(tableRow, searchInput) {
+  let inputValue = searchInput.value.toLowerCase().trim();
+  tableRow.forEach((row) => {
+    let fullName = row.children[1].textContent.toLowerCase();
+    if (fullName.includes(inputValue)) {
+      row.style.display = "";
+    } else {
+      row.style.display = "none";
     }
-    else if(addBtn.classList.contains("addTeacher")){
-        document.querySelector("#teacherRadio").checked = true;
-    }
-    addUserModal.showModal();
-})
-canceladdUserModalBtn.addEventListener("click",()=>{
-    addUserModal.close();
-})
+  });
+}
+
+//Main
+addBtn.addEventListener("click", () => {
+  if (addBtn.classList.contains("addStudent")) {
+    document.querySelector("#studentRadio").checked = true;
+  } else if (addBtn.classList.contains("addTeacher")) {
+    document.querySelector("#teacherRadio").checked = true;
+  }
+  addUserModal.showModal();
+});
+canceladdUserModalBtn.addEventListener("click", () => {
+  addUserModal.close();
+});
+searchInput.addEventListener("keyup", () => {
+  filter(tableRow, searchInput);
+});
